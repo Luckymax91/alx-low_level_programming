1173f04001d522f47d9895f16e50023c5a4cad42
Lucky Enakarhire <maxluckymaxmax@gamil.com>
2022-09-08 16:59:05 -0700

    Hello,put

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+vim 4-puts.c
+
+#include <stdio.h>
+
+/**
+ * main - Print
+ *
+ * Return: Always 0 (Successful)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
