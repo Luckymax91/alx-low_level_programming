6ce55b46f958144059c1a11aba60311a4347c0c7
Lucky Enakarhire <maxluckymaxmax@gamil.com>
2022-09-15 05:46:44 -0700

    4-puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,13 @@
-#include <studios.h>
+#include <stdio.h>
+
 /**
- *main - main block
- *Return:0
+ * main -Printing
+ *
+ * Return: Always 0 (Successful)
  */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return(0);
+	return (0);
 }
 
